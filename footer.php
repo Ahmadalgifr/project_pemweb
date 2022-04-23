78c1064788758d93da4c1a7c60e76c95d4952502
Ahmadalgifr <91266701+Ahmadalgifr@users.noreply.github.com>
2022-04-23 23:20:53 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,20 @@
+    <!-- Main Footer -->
+    <footer class="main-footer">
+        <!-- To the right -->
+        <div class="float-right d-none d-sm-inline">LAIN-NYA</div>
+        <!-- Default to the left -->
+        <strong>Design By &copy;2022 <a href="https://adminlte.io">AHMAD ALGIFARI</a>.</strong>
+      </footer>
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+
+    <!-- jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+include_once 'header.php';
+include_once 'sidebar.php';
+?>
+
+<div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+          <div class="container-fluid">
+            <div class="row mb-2">
+              <div class="col-sm-6 p-5 jumbroton bg-primary">
+                <h1 class="m-0">WELCOME </h1>
+                <h1 class="m-0"> Silahkan Pilih Praktikum atau Project!</h1>
+              </div>
+              <!-- /.col -->
+              <div class="col-sm-6">
+               
+              </div>
+              <!-- /.col -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <div class="content">
+          <div class="container-fluid">
+            <div class="row">
+              
+            </div>
+
+            <!-- /.row -->
+          </div>
+          <!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
+      </div>
+<?php
+include_once 'footer.php';
+?>
